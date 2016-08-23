65500f4646f25c17ada874da1445a46ea496baf6
stay4it2geek <stay4it2geek@sina.com>
2016-08-23 13:58:23 +0800

    Update MainActivity.java

--- app/src/main/java/com/smartphone/imjx/verticalscrolltext/MainActivity.java
+++ app/src/main/java/com/smartphone/imjx/verticalscrolltext/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
 
     private int num = 0;
 
-    private String[] mStrData = {"苏宁看不下去，全场免费送！", "58同城来凑热闹!", "京东大促,全场5元!", "天猫不服,全场不要钱!", "苏宁看不下去，全场免费送！"};
+    private String[] mStrData = {"点击查看位置", "苏宁看不下去，全场免费送！", "58同城来凑热闹!", "京东大促,全场5元!", "天猫不服,全场不要钱!", "苏宁看不下去，全场免费送！"};
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
@@ -35,13 +35,13 @@ public class MainActivity extends Activity {
         for (int i = 0; i < mStrData.length; i++) {
             final View contentView = getLayoutInflater().inflate(R.layout.activity_content, null);
             TextView textView = (TextView) contentView.findViewById(R.id.textView);
-            textView.setText("我是第" + i + "个textview");
+            textView.setText(mStrData[i]);
             contentView.setTag(i);
             layout.addView(contentView, tparams);
             contentView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this,contentView.getTag()+"",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"点击了第"+contentView.getTag()+"个",Toast.LENGTH_SHORT).show();
                 }
             });
         }
